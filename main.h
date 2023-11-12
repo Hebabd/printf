433e7c88276b0a418335b59c26adaa87fae73ad2
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-12 09:34:45 +0000

    Edit main.h header file

--- main.h
+++ main.h
@@ -1,23 +1,28 @@
-#ifndef _MAIN_
-#define _MAIN_
+#ifndef MAIN
+#define MAIN
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct type - Struct type.
+ *
+ * @specifier: The specifier.
+ * @f: The function associated.
+ */
+
 typedef struct format
 {
-	char *id;
-	int (*f)();
-}  match;
+	char *specifier;
+	int (*f)(va_list); 
+} match;
 
-int _printf(const char *format, ...);
-int _putchar(char c);
-int printf_char(va_list avl);
-int printf_string(va_list val);
-int _strlen(char *str);
-int _strlenc(const char *str);
-int print_37(void);
+int print_numbers(unsigned int num);
+int count_numbers(unsigned int num);
+int print_digit(va_list list);
+int (*get_function(const char *specifier))(va_list);
 
-#endif /* _MAIN_ */
+
+#endif /* MAIN */
