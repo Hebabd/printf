1792864371a628b3e7529be14fb9d35e8d735f9e
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-11-13 15:30:44 +0000

    Restore pointer function

--- print_pointer.c
+++ print_pointer.c
@@ -10,18 +10,19 @@
 int print_pointer(va_list list)
 {
 	void *p;
-	/* char *s = "(nil)"; */
+	char *s = "(nil)";
 	long int a;
-	int /*i,*/ b;
+	int i, b;
 
 	p = va_arg(list, void *);
+
 	if (p == NULL)
 	{
-		/* for (i = 0; s[i] != '\0'; i++)
+		for (i = 0; s[i] != '\0'; i++)
 			_putchar(s[i]);
-		return (i); */
-		return (-1);
+		return (i); 
 	}
+
 	a = (unsigned long int)p;
 	_putchar('0');
 	_putchar('x');
